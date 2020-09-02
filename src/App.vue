0fa0771a9ef1ff5a9738ab4f8efbbc3310c3d2e9
Tyler Morales <moratyle@gmail.com>
2020-09-02 16:50:49 -0500

    ✨ Create Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavBar />
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import NavBar from '@/components/NavBar'
+export default {
+  components: {
+    NavBar
   }
+}
+</script>
+
+<style lang="scss">
+// styles go here
+#app {
+  height: 2000px;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,120 @@
+<template>
+  <nav class="nav" :class="{ 'hidden-navbar': !showNavbar }">
+    <div class="nav__links">
+      <router-link to="/" class="nav__name">
+        <span class="title">Tyler Morales</span>
+        <span class="sub-title">Front End Developer</span>
+      </router-link>
+      <router-link to="/about" class="nav__about">About</router-link>
+      <router-link to="/projects" class="nav__projects">Projects</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+const OFFSET = 30 //distance to evoke nav bar
+
+export default {
+  data() {
+    return {
+      showNavbar: true,
+      lastScrollPosition: 0,
+      scrollValue: 0
+    }
+  },
+
+  mounted() {
+    this.lastScrollPosition = window.pageYOffset
+    window.addEventListener('scroll', this.onScroll)
+    const viewportMeta = document.createElement('meta')
+    viewportMeta.name = 'viewport'
+    viewportMeta.content = 'width=device-width, initial-scale=1'
+    document.head.appendChild(viewportMeta)
+  },
+
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+
+  methods: {
+    onScroll() {
+      if (window.pageYOffset < 0) {
+        return
+      }
+      if (Math.abs(window.pageYOffset - this.lastScrollPosition) < OFFSET) {
+        return
+      }
+      this.showNavbar = window.pageYOffset < this.lastScrollPosition
+      this.lastScrollPosition = window.pageYOffset
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.nav.hidden-navbar {
+  box-shadow: none;
+  transform: translate3d(0, -100%, 0);
+}
+.nav {
+  transform: translate3d(0, 0, 0);
+  font-family: var(--font-main);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 12px 0;
+  color: var(--white);
+  position: fixed;
+  background-color: $color-primary;
+  font-size: 14px;
+  transition: all 0.3s;
+  z-index: 11;
+  font-weight: 400;
+  width: 100vw;
+
+  &__links {
+    min-width: 400px;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+
+    @include breakpoint(sm) {
+      min-width: 300px;
+    }
+  }
+
+  @include breakpoint(sm) {
+    margin-bottom: 0;
+    padding: 5px 0;
+  }
+
+  &__name {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    order: 2;
+    border: 1px solid white;
+    padding: 5px 15px;
+    transition: all 0.3s;
+    font-size: 18px;
+
+    &:hover {
+      color: var(--black);
+      background-color: var(--white);
+    }
+
+    .sub-title {
+      font-size: 12px;
+      margin-top: 5px;
+    }
+  }
+
+  &__about {
+    order: 3;
+  }
+
+  &__projects {
+    order: 1;
+  }
+}
+</style>
