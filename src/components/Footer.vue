842eb98a47b788c4246181788f267ae3de533710
Tyler Morales <moratyle@gmail.com>
2020-09-03 18:17:02 -0500

    ✨ Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,55 @@
+<template>
+  <footer>
+    <div class="logo">
+      <span>Tmo</span>
+      <span>Codes</span>
+    </div>
+    <div class="links">
+      <a href="#projects">Projects</a>
+      <a href="#about">About</a>
+      <a href="mailto:moratyle@gmail.com">Contact</a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  max-width: 75vw;
+  margin: 50px auto;
+  font-size: 16px;
+
+  .logo {
+    font-family: 'IBM Plex Sans', sans-serif !important;
+    font-weight: 700;
+    text-transform: uppercase;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .links {
+    display: flex;
+    gap: 20px;
+
+    & > * {
+      font-size: 16px;
+    }
+  }
+
+  @include breakpoint(md) {
+    padding: 50px;
+    margin: 0 auto;
+    max-width: 90vw;
+  }
+
+  @include breakpoint(sm) {
+    padding: 20px;
+  }
+}
+</style>
