81297f823429071d47743cc981526d209f4590c2
Tyler Morales <moratyle@gmail.com>
2020-09-13 13:30:00 -0500

    💫  Add page transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <NavBar />
-    <router-view />
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
     <Footer />
   </div>
 </template>
@@ -19,5 +21,16 @@ export default {
 
 <style lang="scss">
 #app {
+  .fade-enter-active,
+  .fade-leave-active {
+    transition-duration: 0.3s;
+    transition-property: opacity;
+    transition-timing-function: ease;
+  }
+
+  .fade-enter,
+  .fade-leave-active {
+    opacity: 0;
+  }
 }
 </style>
