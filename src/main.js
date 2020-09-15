f62a00642f137051048166bf08459eab10110e1f
Tyler Morales <moratyle@gmail.com>
2020-09-15 15:55:44 -0500

    📦  Add AOS package

--- src/main.js
+++ src/main.js
@@ -5,10 +5,16 @@ import router from './router'
 import VueGlide from 'vue-glide-js'
 import 'vue-glide-js/dist/vue-glide.css'
 
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 Vue.use(VueGlide)
 Vue.config.productionTip = false
 
 new Vue({
+  created() {
+    AOS.init()
+  },
   router,
   render: h => h(App)
 }).$mount('#app')
